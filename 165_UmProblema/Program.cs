5c7ec30290e2b4b5cc4b4dbfb64e2e96bd24e2a1
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-10-12 09:03:23 -0600

    Aula 165 - Um problema que voce nao sabia que tinha

---
+++ 165_UmProblema/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _165_UmProblema
+{
+    struct Player
+    {
+        public int Coins;
+
+        public void ExpendCoins()
+        {
+            Coins--;
+        }
+    }
+
+    struct UI
+    {
+        public Player Player;
+
+        public void ShowCoins()
+        {
+            Console.WriteLine($"Voce tem {Player.Coins} coins");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player
+            {
+                Coins = 10
+            };
+
+            UI ui = new UI
+            {
+                Player = player
+            };
+
+            ui.ShowCoins();
+            Console.WriteLine();
+            while (player.Coins > 0)
+            {
+                Console.WriteLine("Aperte qualquer tecla para gastar coins");
+                Console.ReadKey();
+                Console.WriteLine();
+                player.ExpendCoins();
+                ui.ShowCoins();
+            }
+            Console.WriteLine("\n\n---- Fim do programa ----\n\n");
+            Console.ReadKey();
+        }
+    }
+}
