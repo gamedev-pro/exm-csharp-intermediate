6c05776ce8bdb8a0abb3f08938c4ac89a6a41177
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-10-31 17:16:03 -0600

    206 - Exceptions

---
+++ 206_Exceptions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _206_Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] numbers = new int[] { 1, 3, 4 };
+                Console.WriteLine(numbers[0]);
+                // Console.WriteLine(numbers[5]);
+                int x = 0;
+                float y = 2.5f * x;
+
+                string n = null;
+                int index = n.IndexOf('2');
+            }
+            catch (System.IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"Index Out of Range {e}");
+            }
+            catch (System.NullReferenceException e)
+            {
+                Console.WriteLine($"Index Out of Range {e}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
